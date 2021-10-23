90b7871c7a6f0adb1e8b0af3bf57893037276089
Guruprasad Kamath <guru241987@gmail.com>
2021-10-23 09:16:10 +0200

    Untested: Update Display Error Messages

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -1,19 +1,20 @@
-// var Contract = require('web3-eth-contract');
 import Web3 from 'web3';
-import { getMethodList } from './utils';
-const contractJSON = require("../contracts/SimpleStorage.json"); 
+import { getMethodList, defineFunction, handleErrorMessage } from './utils';
+const contractJSON = require("../contracts/SecretAuction.json"); 
 
 
 let selectedAccount;
 let contractInstance;
+let web3;
 
 export const clientInit = async function(){
 
     const provider = 'http://localhost:8545';
 
-    const web3 = new Web3(provider);
+    web3 = new Web3(provider);
 
-    const accountsList = await web3.eth.getAccounts();
+    const accountsList = await web3.eth.getAccounts()
+                            .catch(e => console.log('Error Web3 Accounts: ', e.message));
     selectedAccount = accountsList[0];
     contractInstance = new web3.eth.Contract(contractJSON.abi, 
                                 contractJSON.networks[5777].address,
@@ -25,32 +26,9 @@ export const clientInit = async function(){
 const initiateMethodList = getMethodList(contractJSON);
 
 initiateMethodList.forEach((element) => {
-    element.functionDef = async function(inputs){
-        if (!contractInstance){
-            return "Please Connect a Client";
-        }
-        let result; 
-        if (element.callorsend === "call") {
-            if (element.noInputs){
-                result = await contractInstance.methods[element.textSignature]().call();
-            } else {
-                result = await contractInstance.methods[element.textSignature](...inputs).call();
-            }
-            
-        } else if (element.callorsend === "send") {
-            let t;
-            if (element.noInputs) {
-                t = await contractInstance.methods[element.textSignature]().send();
-            } else {
-                t = await contractInstance.methods[element.textSignature](...inputs).send();
-            }
-            result = t.transactionHash;
-            
-        }
-
-        return result;
-    
-    }
+    element.functionDef = function(inputs){
+        return defineFunction(inputs, contractInstance, element);
+    }   
 })
 
 export const contractMethodList = initiateMethodList;
--- client/src/components/utils.js
+++ client/src/components/utils.js
@@ -14,16 +14,20 @@ export const getMethodList = function(contract) {
         methodDefinition.name = element.name;
         methodDefinition.noInputs = false;
         let methodInputs = "";
+        let inputNames = "";
         element.inputs.forEach((el) => {
             if (methodInputs === "") {
                 methodInputs = el.type;
+                inputNames = el.type + " " + el.name;
             } else {
                 methodInputs = methodInputs + "," + el.type;
+                inputNames = inputNames + "; " + el.type + " " + el.name;
             }
         })
         if (methodInputs === ""){
             methodDefinition.noInputs = true;
         }
+        methodDefinition.inputNames = inputNames;
         methodDefinition.textSignature = methodDefinition.name + "(" + methodInputs + ")";
 
         if (callFunctions.includes(element.stateMutability)){
@@ -37,3 +41,63 @@ export const getMethodList = function(contract) {
 
     return methodList;
 }
+
+
+
+const errorString = "VM Exception while processing transaction: ";
+const errorList = [
+    "revert",
+    "out of gas",
+    "invalid JUMP",
+    "invalid opcode",
+    "stack overflow",
+    "stack underflow",
+    "static state change"
+]
+    
+export const handleErrorMessage = function(msg) {
+    let displayMsg = "Unknown Error!";
+    errorList.forEach(element => {
+
+        if (msg.search(errorString + element) >= 0) {
+            displayMsg =  msg.replace(errorString + element, `(${element}) -`);
+        }
+    })
+    return displayMsg;
+}
+
+export const defineFunction = async function(inputs, contractInstance, element){
+
+        const inputArray = inputs[0].split(",");
+        if (!contractInstance){
+            return "Please Connect a Client";
+        }
+        let result = {success: null, error: null}; 
+        if (element.callorsend === "call") {
+            if (element.noInputs){
+
+                result.success = await contractInstance.methods[element.textSignature]().call()
+                        .catch(e => {result.error = handleErrorMessage(e.message)});
+            } else {
+                result.success = await contractInstance.methods[element.textSignature](...inputArray).call()
+                        .catch(e => {result.error = handleErrorMessage(e.message)});
+            }
+            
+        } else if (element.callorsend === "send") {
+            let t;
+            
+            if (element.noInputs) {
+                t = await contractInstance.methods[element.textSignature]().send()
+                        .catch(e => {result.error = handleErrorMessage(e.message)});
+            } else {
+                t = await contractInstance.methods[element.textSignature](...inputArray).send()
+                        .catch(e => {result.error = handleErrorMessage(e.message)});
+            }
+
+            if (!result.error){ result.success = t.transactionHash}  
+        }
+
+
+        return result.success ?? result.error;
+    
+    }
